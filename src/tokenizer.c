246f61f6906b4bf6a6b7853107e5bfd2ecbe4400
edbrgtz <ebriones1@miners.utep.edu>
2022-09-21 08:10:59 -0600

    Added makefile

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -7,15 +7,20 @@
 int space_char(char c){
     if (c == ' ' || c == '\t'){
         return 1;
-    } else{
-        return 0;
     }
+    return 0;
+    
 }
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c){
+    if (c == ' ' || c == '\t'){
+        return 0;
+    }
+    return 1;
+}
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
