74fa22d217d1ce13eb180dbd93b343547118b675
Eduardo Briones <90985690+edbrgtz@users.noreply.github.com>
2022-09-30 09:04:30 -0600

    Fixed history

--- src/history.c
+++ src/history.c
@@ -22,43 +22,53 @@ List* init_history(){
    char* str - the string to store
 */
 void add_history(List *list, char *str){
-    Item tempItem;
-    tempItem.str = list->root;
-    tempItem.next = str;
+    Item *tempItem;
+    Item *newItem;
+    newItem->str = str;
+    tempItem = list->root;
+    
+    while (tempItem) {
+        if (!tempItem->next) {
+            tempItem->next = newItem;
+        }
+        tempItem = tempItem->next;
+    }
+    
 }
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
 char *get_history(List *list, int id){
-    Item tempRoot;
-    tempRoot.str = list->root;
-    while (list->root != NULL) {
-        if(tempRoot.id == id)
-            return tempRoot.str;
+    Item *tempRoot;
+    tempRoot = list->root;
+    while (list->root) {
+        if(tempRoot->id == id)
+            return tempRoot->str;
 
-        tempRoot.str = tempRoot.next;
+        tempRoot = tempRoot->next;
     }  
 }
 
 /*Print the entire contents of the list. */
 void print_history(List *list){
-    Item tempRoot;
-    tempRoot.str = list->root;
+    Item *tempRoot;
+    tempRoot = list->root;
     while (list->root != NULL) {
-        puts(tempRoot.str);
-        tempRoot.str = tempRoot.next;
+        puts(tempRoot->str);
+        puts(tempRoot->id);
+        tempRoot->str = tempRoot->next;
     } 
 }
 
 /*Free the history list and the strings it references. */
 void free_history(List *list){
-    Item tempRoot;
-    tempRoot.str = list->root;
-    if (tempRoot.next == NULL){
-        list->root = NULL;
-    } else {
-        list->root = tempRoot.next;
-        free_history(list);
+    while(list->root){
+        Item *p = list->root;
+        list->root = list->root->next;
+        Free(p->str);
+        Free(p->next);
+        Free(p->id);
+        Free(p);
     }
 }
